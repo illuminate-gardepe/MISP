11a8d55861c7e6fa40a090dde24e7a1fc0a92acd
Gardepe <mgardepe@Illuminate.Solutions>
2021-01-13 13:31:22 -0500

    Cert path change

--- app/Model/Datasource/Database/MysqlExtendedLogging.php
+++ app/Model/Datasource/Database/MysqlExtendedLogging.php
@@ -166,7 +166,7 @@ class MysqlExtendedLogging extends DboSource {
 			PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 			PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-			PDO::MYSQL_ATTR_SSL_CA => "/etc/nginx/dhparam.pem"
+			PDO::MYSQL_ATTR_SSL_CA => "/etc/nginx/certs/dhparam.pem"
 		);
 
 		if (!empty($config['encoding'])) {
